125e6ae2d4784ff2e37311e9b0b12e55458ee207
Kirill Shevchenko <kir971@gmail.com>
2021-10-27 20:10:48 +0300

    Add project files.

---
+++ 20211025_StackImplementation/MyStack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20211025_StackImplementation
+{
+    class MyStack<T>
+    {
+        Entry _top;
+
+        public void Push(T data)
+        {
+            _top = new Entry(_top, data);
+        }
+
+        public T Pop()
+        {
+            if (_top == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            T result = _top.data;
+            _top = _top.next;
+
+            return result;
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return _top == null;
+            }
+        }
+
+        class Entry
+        {
+            public Entry next;
+            public T data;    
+
+            public Entry(Entry next, T data)
+            {
+                this.next = next;
+                this.data = data;
+            }
+        }
+    }
+}
---
+++ 20211025_StackImplementation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20211025_StackImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(6);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            if(stack.Count != 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+
+            MyStack<double> myStack = new MyStack<double>();
+
+            myStack.Push(2.23);
+            myStack.Push(33.2);
+
+            Console.WriteLine(myStack.Pop());
+            Console.WriteLine(myStack.Pop());
+
+            if(!myStack.IsEmpty)
+            {
+                Console.WriteLine(myStack.Pop());
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
